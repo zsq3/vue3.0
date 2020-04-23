8cd5c7b54f93dbb6a8bfabb4fdf93f159db7f2ad
泉 <1259437457@qq.com>
2020-04-23 15:24:51 +0800

    新增provide inject 生命周期新写法

---
+++ src/components/Inject.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        provide-inject测试接收到得数据为： {{ order }}
+    </div>
+</template>
+
+<script>
+    import { inject } from '@vue/composition-api'
+
+    export default {
+        setup(props, ctx){
+            const order = inject('order');
+
+            return { order }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Setup.vue
+++ src/components/Setup.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
         <h3>接收到的父组件数据： {{page}}</h3>
+        <ject-test></ject-test>
     </div>
 </template>
 
 <script>
-    export default {
+  import {provide, onMounted, onUpdated, onUnmounted} from '@vue/composition-api';
+  import Ject from "@/components/Inject";
+
+
+  export default {
+      components: {
+         "ject-test": Ject
+      },
       props: {
           page: Number
       },
@@ -15,6 +23,15 @@
 
       setup(props, ctx){  //执行顺序在 beforeCreate和created 之间
         console.log(props.page, ctx); //setup中无法访问this, 用 ctx 代替
+        provide('order', '上级传给下级得数据');
+
+        onMounted(() => {
+           //ctx.refs.xx
+        });
+        onUnmounted(() => {
+           //clearInterval(ctx.xx.timer)
+        })
+
       },
 
       created(){
